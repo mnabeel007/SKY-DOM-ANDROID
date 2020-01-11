1a0765451bb41e96064cc30827a27f1f06911666
Nabeel Mohammed <mnabeel007@gmail.com>
2020-01-11 20:05:01 +0530

    fourth final

---
+++ app/src/main/java/k/ccet_nabeel/hostel/page4.java
@@ -0,0 +1,14 @@
+ package k.ccet_nabeel.hostel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class page4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_page4);
+    }
+}
